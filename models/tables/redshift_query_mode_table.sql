008b497dd3190cea60bccb3a4458799079ebea99
David Wallace <dwallace0723@gmail.com>
2017-12-11 18:34:53 -0800

    add unique key in table

--- models/tables/redshift_query_mode_table.sql
+++ models/tables/redshift_query_mode_table.sql
@@ -1,4 +1,6 @@
-select *
+select
+  md5(query_id::varchar + '-' + started_at::varchar) as unique_key,
+  *
 from {{ ref('redshift_query_mode') }}
 {% if adapter.already_exists(this.schema, this.table) and not flags.FULL_REFRESH %}
   where started_at > (select max(started_at) from {{ this }})
