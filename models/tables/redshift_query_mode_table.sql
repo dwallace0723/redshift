e6f179422b173f87784b5aaf1ee851a662ec6395
David Wallace <dwallace0723@gmail.com>
2018-05-09 16:34:36 -0700

    remove tables and add custom schema

--- models/tables/redshift_query_mode_table.sql
+++
@@ -1,5 +0,0 @@
-select *
-from {{ ref('redshift_query_mode') }}
-{% if adapter.already_exists(this.schema, this.table) and not flags.FULL_REFRESH %}
-  where started_at > (select max(started_at) from {{ this }})
-{% endif %}
